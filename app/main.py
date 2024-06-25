9397bcd5a942702321a6826ddd54f0e66d041850
Timur Usmanov <fonuatimur@gmail.com>
2024-06-25 15:44:25 +0700

    add main entrypoint

--- app/main.py
+++ app/main.py
@@ -1,2 +1,14 @@
 from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import SessionLocal, init_db, database
 from scheme import AdScheme
+from models import Ad
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app = FastAPI(title='Advertisments')
